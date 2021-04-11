3bf3076dd9fd5440c1b8b9f802195a0eae82e48a
Matthew Miller <mmlak_0109@yahoo.com>
2021-04-11 23:47:15 +0900

    Add redux store to app

--- App.js
+++ App.js
@@ -2,11 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react'
 import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
+import { createStore } from 'redux'
+import  { Provider } from 'react-redux'
+import reducer from './reducers'
 
 export default function App() {
   return (
-    <View>
-      <AddEntry />
-    </View>
+    <Provider store={createStore(reducer)}>
+      <View>
+        <AddEntry />
+      </View>
+    </Provider>
   );
 }
