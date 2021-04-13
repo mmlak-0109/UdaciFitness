9b26658ef57df749605b17a9517129f993368a1b
Matthew Miller <mmlak_0109@yahoo.com>
2021-04-13 23:51:31 +0900

    Replace AddEntry with History

--- App.js
+++ App.js
@@ -5,12 +5,14 @@ import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import  { Provider } from 'react-redux'
 import reducer from './reducers'
+import History from './components/History'
 
 export default function App() {
   return (
     <Provider store={createStore(reducer)}>
       <View style={{flex: 1}}>
-        <AddEntry />
+        {/* <AddEntry /> */}
+        <History />
       </View>
     </Provider>
   );
