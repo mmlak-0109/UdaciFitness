7da7b71c44869f4aa98ca9f41acf8cc6094350e1
Matthew Miller <mmlak_0109@yahoo.com>
2021-04-14 23:45:16 +0900

    Render calendar component/write funcs to handle date items

--- components/History.js
+++ components/History.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 import { receiveEntries, addEntry } from '../actions'
 import { timeToString, getDailyReminderValue } from '../utils/helpers'
 import { fetchCalendarResults } from '../utils/api'
-// import Udac
+import { Calendar as UdaciFitnessCalendar } from 'react-native-calendars'
 
 class History extends Component {
 
@@ -22,11 +22,33 @@ class History extends Component {
       })
   }
 
-  render() {
+  renderItem = ({ today, ...metrics }, formattedDate, key) => (
+    <View>
+      {today
+        ? <Text>{JSON.stringify(today)}</Text>
+        : <Text>{JSON.stringify(metrics)}</Text>
+      }
+    </View>
+  )
+
+  renderEmptyDate(formattedDate) {
     return (
       <View>
-        <Text>{JSON.stringify(this.props)}</Text>
+        <Text>No data for this day.</Text>
       </View>
+    )
+  }
+
+  render() {
+    const { entries } = this.props
+
+    return (
+      <UdaciFitnessCalendar
+        items={entries}
+        renderItem={this.renderItem}
+        renderEmptyDate={this.renderEmptyDate}
+      />
+      // <Text>{JSON.stringify(entries)}</Text>
     )
   }
 }
