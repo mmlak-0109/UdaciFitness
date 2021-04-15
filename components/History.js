4222265681b2ff786072d93ff25b0b702574d751
Matthew Miller <mmlak_0109@yahoo.com>
2021-04-15 23:29:30 +0900

    Use AppLoading component w/"app ready" state check

--- components/History.js
+++ components/History.js
@@ -8,8 +8,12 @@ import UdaciFitnessCalendar from 'udacifitness-calendar-fix'
 import { white } from '../utils/colors'
 import DateHeader from './DateHeader'
 import MetricCard from './MetricCard'
+import AppLoading from 'expo-app-loading'
 
 class History extends Component {
+  state = {
+    ready: false,
+  }
 
   componentDidMount() {
     const { dispatch } = this.props
@@ -23,6 +27,9 @@ class History extends Component {
           }))
         }
       })
+      .then(() => this.setState(() => ({
+        ready: true,
+      })))
   }
 
   renderItem = ({ today, ...metrics }, formattedDate, key) => (
@@ -54,6 +61,11 @@ class History extends Component {
 
   render() {
     const { entries } = this.props
+    const { ready } = this.state
+
+    if (ready === false) {
+      return <AppLoading />
+    }
 
     return (
       <UdaciFitnessCalendar
