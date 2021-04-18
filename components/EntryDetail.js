6205652eebee0715dd0675bd2ac6c82eadaf796d
Matthew Miller <mmlak_0109@yahoo.com>
2021-04-18 23:10:17 +0900

    Render entry card and reset button to clear entry from state

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,14 +1,68 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { connect } from 'react-redux'
+import { white } from '../utils/colors'
+import MetricCard from './MetricCard'
+import { addEntry } from '../actions'
+import { removeEntry } from '../utils/api'
+import { timeToString, getDailyReminderValue } from '../utils/helpers'
+import TextButton from './TextButton'
 
 class EntryDetail extends Component {
+  reset = () => {
+    const { remove, goBack, entryId } = this.props
+
+    remove()
+    goBack()
+    removeEntry(entryId)
+  }
+
+  shouldComponentUpdate (nextProps) {
+    return nextProps.metrics !== null && !nextProps.metrics.today
+  }
+
   render() {
+    const { entryId, metrics } = this.props
+
     return (
-      <View>
-        <Text>Entry Detail - {this.props.route.params.entryId}</Text>
+      <View style={styles}>
+        <MetricCard metrics={metrics} />
+        <TextButton onPress={this.reset} style={{margin: 20}}>
+          RESET
+        </TextButton>
       </View>
     )
   }
 }
 
-export default EntryDetail
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    padding: 15,
+  }
+})
+
+function mapStateToProps (state, { route }) {
+  const { entryId } = route.params
+
+  return {
+    entryId,
+    metrics: state[entryId]
+  }
+}
+
+function mapDispatchToProps (dispatch, { route, navigation }) {
+  const { entryId } = route.params
+
+  return {
+    remove: () => dispatch(addEntry({
+      [entryId]: timeToString() === entryId
+        ? getDailyReminderValue()
+        : null
+    })),
+    goBack: () => navigation.goBack(),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EntryDetail)
