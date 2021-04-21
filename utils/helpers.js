c1de9a97b16df481043ca2a254d5e6d8a1195bd1
Matthew Miller <mmlak_0109@yahoo.com>
2021-04-21 23:57:04 +0900

    Update notification funcs to use current API

--- utils/helpers.js
+++ utils/helpers.js
@@ -6,7 +6,6 @@ import { white, red, orange, blue, lightPurp, pink } from './colors'
 import * as Notifications from 'expo-notifications'
 import * as Permissions from 'expo-permissions'
 
-
 const NOTIFICATION_KEY = 'UdaciFitness:notifications'
 
 export function isBetween (num, x, y) {
@@ -170,45 +169,61 @@ export function getDailyReminderValue () {
 
 export function clearLocalNotification () {
   return AsyncStorage.removeItem(NOTIFICATION_KEY)
-    .then(Notifications.cancelAllScheduledNotificationsAsync)
+    .then(() => {
+      Notifications.cancelAllScheduledNotificationsAsync(),
+      console.log("Notification cleared for: ", new Date().toLocaleDateString())
+    })
 }
 
-function createNotification () {
+function createNotification (dateTime) {
   return {
-    title: 'Log your stats!',
-    body: "Don't forget to log your stats",
-    ios: {
-      sound: true
-    },
-    android: {
+    content: {
+      title: 'Log your stats!',
+      body: "Don't forget to log your stats for today!",
       sound: true,
-      priority: 'high',
-      sticky: false,
       vibrate: true,
+      priority: 'high',
+    },
+    trigger: dateTime
     }
-  }
 }
 
 export function setLocalNotification () {
   AsyncStorage.getItem(NOTIFICATION_KEY)
-    .then(({ status }) => {
-      if (status === 'granted') {
-        Notifications.cancelAllScheduledNotificationsAsync()
+    .then(JSON.parse)
+    .then((data) => {
+      console.log("notif key data:", data);
+      if (data === null) {
+        Notifications.requestPermissionsAsync()
+          .then(({ status }) => {
+            console.log("permi status: ", status);
+            if (status === 'granted') {
+              console.log("Permission Granted");
+              Notifications.cancelAllScheduledNotificationsAsync()
 
-        let tomorrow = new Date()
-        tomorrow.setDate(tomorrow.getDate() + 1)
-        tomorrow.setHours(20)
-        tomorrow.setMinutes(0)
-
-        Notifications.scheduleNotificationAsync(
-          createNotification(),
-          {
-            time: tomorrow,
-            repeat: 'day',
-          }
-        )
-
-        AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+              Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                  shouldShowAlert: true,
+                  shouldPlaySound: false,
+                  shouldSetBadge: false,
+                }),
+              });
+      
+              let tomorrow = new Date()
+              // tomorrow = tomorrow.getTime() + (10 * 1000)
+              // const notificationDate = new Date(tomorrow)
+              tomorrow.setDate(tomorrow.getDate())
+              tomorrow.setHours(20)
+              tomorrow.setMinutes(0)
+      
+              Notifications.scheduleNotificationAsync(
+                createNotification(tomorrow),
+                // createNotification(notificationDate),
+              )
+      
+              AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+            }
+          })
       }
     })
 }
